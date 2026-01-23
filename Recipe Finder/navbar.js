0041767a260e40d7d60ee29be58a7dfa8aac08eb
stavan24 <stavanhalde@gmail.com>
2026-01-23 23:19:13 +0530

    Update navbar.js

--- Recipe Finder/navbar.js
+++ Recipe Finder/navbar.js
@@ -1,10 +1,17 @@
-function toggleMenu() {
-  document.querySelector(".nav-links").classList.toggle("active");
-}
+const hamburger = document.getElementById("hamburger");
+const mobileMenu = document.getElementById("mobileMenu");
 
+hamburger.addEventListener("click", () => {
+  mobileMenu.classList.toggle("active");
+});
+
+/* Dark mode */
 function toggleDark() {
   document.body.classList.toggle("dark");
-  localStorage.setItem("dark", document.body.classList.contains("dark"));
+  localStorage.setItem(
+    "dark",
+    document.body.classList.contains("dark")
+  );
 }
 
 if (localStorage.getItem("dark") === "true") {
